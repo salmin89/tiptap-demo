ec1613784acc0b53d593c8b0b13bb6f6485a3759
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-11-15 08:49:46 -0500

    basic demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Editor } from '@tiptap/core';
 
+import Document from '@tiptap/extension-document';
+import Paragraph from '@tiptap/extension-paragraph';
+import Text from '@tiptap/extension-text';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +12,12 @@ import { Editor } from '@tiptap/core';
 })
 export class AppComponent {
   editor = new Editor({
-    extensions: [],
+    extensions: [
+      // basic required extensions
+      Document,
+      Paragraph,
+      Text,
+    ],
   });
 
   value = '<p>Hello, Tiptap!</p>'; // can be HTML or JSON, see https://www.tiptap.dev/api/editor#content
